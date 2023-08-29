7474234c2c993a454d478ea32c3e530233989a19
hakanalphan <78258929+hakanalphan@users.noreply.github.com>
2023-08-29 16:03:18 +0300

    ...

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavItem, NavLink, Badge } from "reactstrap";
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Badge } from "reactstrap";
 import { useDispatch, useSelector } from "react-redux";
 import * as cartActions from "../../redux/actions/CartAction";
 import { Link } from "react-router-dom";
@@ -7,17 +7,15 @@ import alertify from "alertifyjs";
 
 const CartSummary = () => {
   const dispatch = useDispatch();
-  const cartItems = useSelector((state) => state.cartItems);
+  const cartItems = useSelector((state) => state.cart.cartItems);
 
   const removeFromCart = (product) => {
     dispatch(cartActions.removeFromCart(product));
-    alertify.error(product.productName + " sepetten silindi!", 2);
+    alertify.error(`${product.productName} sepetten silindi!`, 2);
   };
 
   const renderEmpty = () => (
-    <NavItem>
-      <NavLink>Sepetiniz boş</NavLink>
-    </NavItem>
+    <div>Sepetiniz boş</div>
   );
 
   const renderSummary = () => (
@@ -28,14 +26,14 @@ const CartSummary = () => {
       <DropdownMenu right>
         {cartItems.map((cartItem) => (
           <DropdownItem key={cartItem.product.id}>
-            <Badge onClick={() => removeFromCart(cartItem.product)} color="danger">-</Badge>
-            {cartItem.product.productName}
+            <Badge onClick={() => removeFromCart(cartItem.product)} color="danger">-</Badge>{" "}
+            {cartItem.product.productName}{" "}
             <Badge color="success">{cartItem.quantity}</Badge>
           </DropdownItem>
         ))}
 
         <DropdownItem divider />
-        <DropdownItem><Link to={"/cart"}>Sepete Git</Link></DropdownItem>
+        <DropdownItem tag={Link} to="/cart">Sepete Git</DropdownItem>
       </DropdownMenu>
     </UncontrolledDropdown>
   );
